1bc99cfdc4ab3c3fc6872edb95e1a25918c4bda7
John D'Agostino <jdagos3@gmail.com>
2017-05-30 15:17:15 -0600

    dist stuff

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -20,20 +20,6 @@ angular.module('vimeoApp', ["ui.router"]).config(["$stateProvider", "$urlRouterP
         url: '/edit',
         templateUrl: '../views/editvideo.html',
         controller: 'editCtrl'
-<<<<<<< HEAD
-    }).state('playVideo', {
-        url: '/playVideo',
-        templateUrl: 'views/playVideo.html',
-        controller: 'playVideoCtrl'
-    }).state('uploadVideo', {
-        url: '/uploadVideo',
-        templateUrl: 'views/uploadVideo.html',
-        controller: 'uploadVideoCtrl'
-    }).state('upload', {
-        url: '/upload',
-        templateUrl: './views/upload.html',
-        controller: 'uploadCtrl'
-=======
     }).state('playvideo', {
         url: '/playvideo',
         templateUrl: './views/playVid.html',
@@ -42,16 +28,11 @@ angular.module('vimeoApp', ["ui.router"]).config(["$stateProvider", "$urlRouterP
         url: '/uploadVideo',
         templateUrl: './views/uploadVideo.html',
         controller: 'uploadVideoCtrl'
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
     });
 }]);
 'use strict';
 
-<<<<<<< HEAD
-angular.module('vimeoApp').controller('editCtrl', ["$scope", function ($scope) {}]);
-=======
 angular.module('vimeoApp').controller('accountCtrl', ["$scope", function ($scope) {}]);
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 'use strict';
 
 angular.module('vimeoApp').controller('mainCtrl', ["$scope", "mainService", function ($scope, mainService) {
@@ -62,8 +43,6 @@ angular.module('vimeoApp').controller('mainCtrl', ["$scope", "mainService", func
         });
     };
     $scope.login();
-<<<<<<< HEAD
-=======
 
     // mainService.getVideosByChannel('staffpicks').then(res => {
     //     $scope.staffpicks = res.data;
@@ -76,7 +55,6 @@ angular.module('vimeoApp').controller('mainCtrl', ["$scope", "mainService", func
     // mainService.getVideosByChannel('animation').then(res => {
     //     $scope.animation = res.data;
     // })
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 }]);
 'use strict';
 
@@ -86,53 +64,33 @@ angular.module('vimeoApp').controller('navBarCtrl', ["$scope", "mainService", "$
         $state.go('home');
         mainService.searchVideos(1, query).then(function (response) {
             mainService.searchedVideo(response.data.data);
-<<<<<<< HEAD
-=======
             console.log(response.data.data);
             // let videoId = response.data.data.replace(/\D/g, '')
             // mainService.id.push(videoId);
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
             $state.go('search');
             $scope.query = '';
         });
     };
     $scope.getUser = function () {
-<<<<<<< HEAD
-        mainService.getUser().then(function (res) {
-            console.log(res.data); //res.data is the currently logged-in user's info
-            $state.go('upload');
-=======
         $state.go('uploadVideo');
         mainService.getUser().then(function (res) {
             console.log(res.data); //res.data is the currently logged-in user's info
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
         });
     };
 }]);
 'use strict';
 
-<<<<<<< HEAD
-angular.module('vimeoApp').controller('playVideoCtrl', ["$scope", "mainService", function ($scope, mainService) {
-    $scope.video = mainService.video;
-
-    var id = mainService.id.replace(/\D/g, '');
-=======
 angular.module('vimeoApp').controller('playVideo', ["$scope", "mainService", function ($scope, mainService) {
     $scope.video = mainService.video;
 
     var id = mainService.arr[0];
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 
     mainService.getComments(id).then(function (res) {
         $scope.comments = res.data.data;
         console.log($scope.comments);
     });
-<<<<<<< HEAD
-    document.querySelector(".videoHolder").innerHTML = $scope.video;
-=======
     document.querySelector(".video-window").innerHTML = $scope.video;
     console.log($scope.video);
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 }]);
 'use strict';
 
@@ -147,34 +105,16 @@ angular.module('vimeoApp').controller('searchCtrl', ["$scope", "mainService", "$
         mainService.getId(id);
     };
 
-<<<<<<< HEAD
-    $scope.playVideo = function (videoLink) {
-        mainService.clickedVideo(videoLink);
-        $state.go('playVideo');
-=======
     $scope.playVideo = function (videoLink, uri) {
         mainService.clickedVideo(videoLink);
         var id = uri.replace(/\D/g, '');
         mainService.getId(id);
         $state.go('playvideo');
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
     };
 
     $scope.page = function (num) {
         mainService.searchVideos(num, mainService.query).then(function (res) {
             $scope.videos = res.data.data;
-<<<<<<< HEAD
-        });
-    };
-}]);
-'use strict';
-
-angular.module('vimeoApp').controller('uploadCtrl', ["$scope", "mainService", "$state", function ($scope, mainService, $state) {
-    $scope.upload = function () {
-        mainService.uploadVideo($scope.video).then(function (res) {
-            console.log(res.data);
-            $state.go('userVideos');
-=======
             console.log($scope.videos);
         });
     };
@@ -187,18 +127,11 @@ angular.module('vimeoApp').controller('uploadVideoCtrl', ["$scope", "mainService
         mainService.uploadVideo().then(function (res) {
             console.log(res.data);
             $scope.link = JSON.stringify(res.data);
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
         });
     };
 }]);
 'use strict';
 
-<<<<<<< HEAD
-angular.module('vimeoApp').controller('uploadVideoCtrl', ["$scope", function ($scope) {}]);
-'use strict';
-
-=======
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 angular.module('vimeoApp').controller('userVideosCtrl', ["$scope", "mainService", function ($scope, mainService) {
     $scope.userVideos = function () {
         mainService.userVideos().then(function (res) {
@@ -207,26 +140,6 @@ angular.module('vimeoApp').controller('userVideosCtrl', ["$scope", "mainService"
         });
     };
 }]);
-'use strict';
-
-angular.module('vimeoApp').directive('footerDir', function () {
-
-    return {
-        restrict: "AE",
-        templateUrl: "./views/footerDir.html"
-    };
-});
-'use strict';
-
-angular.module('vimeoApp').directive('navBar', function () {
-
-  return {
-    restrict: 'E',
-    templateUrl: './views/navBar.html',
-    link: function link(scope) {},
-    controller: 'navBarCtrl'
-  };
-});
 'use strict';
 
 angular.module('vimeoApp').service('mainService', ["$http", function ($http) {
@@ -239,17 +152,10 @@ angular.module('vimeoApp').service('mainService', ["$http", function ($http) {
     this.searchedVideo = function (data) {
         this.videoData = data;
     };
-<<<<<<< HEAD
-    this.id = '';
-
-    this.getId = function (id) {
-        _this.id = id;
-=======
     this.arr = [];
 
     this.getId = function (id) {
         _this.arr.push(id);
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
     };
 
     this.video = '';
@@ -296,17 +202,9 @@ angular.module('vimeoApp').service('mainService', ["$http", function ($http) {
             url: serverUrl + '/api/currentuser'
         });
     };
-<<<<<<< HEAD
-    this.uploadVideo = function (video) {
-        console.log(video);
-        return $http({
-            method: 'POST',
-            data: { video: video },
-=======
     this.uploadVideo = function () {
         return $http({
             method: 'PUT',
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
             url: serverUrl + '/api/upload'
         });
     };
@@ -316,8 +214,6 @@ angular.module('vimeoApp').service('mainService', ["$http", function ($http) {
             url: serverUrl + '/api/usersvideos'
         });
     };
-<<<<<<< HEAD
-=======
 
     this.getVideosByChannel = function (channel) {
         return $http({
@@ -325,6 +221,25 @@ angular.module('vimeoApp').service('mainService', ["$http", function ($http) {
             url: serverUrl + ('/api/videos/channels/' + channel)
         });
     };
->>>>>>> 9ca3fd12baeb3fd8773a465fed677fa271ab062a
 }]);
+'use strict';
+
+angular.module('vimeoApp').directive('footerDir', function () {
+
+    return {
+        restrict: "AE",
+        templateUrl: "./views/footerDir.html"
+    };
+});
+'use strict';
+
+angular.module('vimeoApp').directive('navBar', function () {
+
+  return {
+    restrict: 'E',
+    templateUrl: './views/navBar.html',
+    link: function link(scope) {},
+    controller: 'navBarCtrl'
+  };
+});
 //# sourceMappingURL=bundle.js.map
