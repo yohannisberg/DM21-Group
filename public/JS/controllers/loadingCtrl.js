aa27004d3b28eed735bf52fa3bff5e20c37c6de7
John D'Agostino <jdagos3@gmail.com>
2017-06-01 13:58:06 -0600

    miscellaneous things corrected

---
+++ public/JS/controllers/loadingCtrl.js
@@ -0,0 +1,21 @@
+angular.module('vimeoApp').controller('loadingCtrl', function ($scope, $timeout) {
+
+$scope.loadB=true;
+$scope.loadG=false;
+$scope.loadR=false;
+
+$timeout();
+
+$timeout(function() {
+  $scope.loadG=true;
+}, 500);
+
+$timeout();
+
+$timeout(function() {
+  $scope.loadR=true;
+}, 1000);
+
+$timeout();
+
+});
